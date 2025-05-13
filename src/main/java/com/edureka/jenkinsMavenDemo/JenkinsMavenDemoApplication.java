d02bd09bfec2b1c2fe681214a62dd61380f74e38
Sabarinadh <sabarinadh1720@gmail.com>
2025-05-14 00:40:14 +0530

    Initialpush

---
+++ src/main/java/com/edureka/jenkinsMavenDemo/JenkinsMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.edureka.jenkinsMavenDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMavenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edureka/jenkinsMavenDemo/JenkinsMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.jenkinsMavenDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
